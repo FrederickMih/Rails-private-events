8f2e4eabfa3fcbf705f9e6f923d9a03e1629a917
FrederickMih <fredtimermih@gmail.com>
2021-03-30 16:20:20 +0100

    Fixed login path

--- db/migrate/20210330150139_userfix.rb
+++ db/migrate/20210330150139_userfix.rb
@@ -1,4 +1,4 @@
-class CreateUsers < ActiveRecord::Migration[6.1]
+class Userfix < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       t.string :username
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_30_104314) do
+ActiveRecord::Schema.define(version: 2021_03_30_150139) do
 
   create_table "event_users", force: :cascade do |t|
     t.integer "user_id"
