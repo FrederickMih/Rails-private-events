b6e85a231ccbdda66a32e339898a444a8c4ae0c0
ayman <aymanjaber2012@gmail.com>
2021-03-30 13:44:17 +0300

    Create tables

---
+++ db/migrate/20210330104107_create_events.rb
@@ -0,0 +1,13 @@
+class CreateEvents < ActiveRecord::Migration[6.1]
+  def change
+    create_table :events do |t|
+      t.string :name
+      t.string :category
+      t.string :location
+      t.string :date
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
