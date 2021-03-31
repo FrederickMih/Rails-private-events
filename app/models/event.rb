49fc9e140891ed47d36e3c93386bfb1db2ba7193
ayman <aymanjaber2012@gmail.com>
2021-03-31 16:47:39 +0300

    Fix issues with association

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,8 +4,10 @@ class Event < ApplicationRecord
     belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
     validates :description, presence: true
 
-    has_many :attendees, through: :event_users, source: :users, dependent: :delete_all
+    
     has_many :event_users
+
+    has_many :attendees, through: :event_users, source: :user, dependent: :delete_all
 
     
     
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ApplicationRecord
     # 4.3.2.9 :source
 
     has_many :events, dependent: :delete_all, inverse_of: 'creator'
-
-    has_many :attended_events, through: :event_users, source: :events, dependent: :delete_all
     has_many :event_users
+
+    has_many :attended_events, through: :event_users, source: :event, dependent: :delete_all
     
 
     
