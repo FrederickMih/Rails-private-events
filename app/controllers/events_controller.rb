2e4527b01df7276e94f703bfea997434d06bcb1c
ayman <aymanjaber2012@gmail.com>
2021-03-31 14:38:42 +0300

    Modify events_controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,10 @@
 class EventsController < ApplicationController
 
   include SessionsHelper
-  # before_action :logged_in_user, only: [:index]
+  before_action :logged_in_user
 
     def new
-        @event = Event.new
+        @event = current_user.events.new
     end
 
     def index 
@@ -16,17 +16,38 @@ class EventsController < ApplicationController
     end
 
     def create
-      # @event = Event.new(event_params)
-      event_creator = current_user
-      @event = event_creator.events.build(event_params)
-      @event.save
-
+      @event = current_user.events.build(event_params)
       if @event.save
           flash[:success] = "Successfully created a new event"
           redirect_to @event
       else
           render :new
       end
+  end
+
+  def edit
+    @event = current_user.events.find(params[:id])
+  end
+
+  def update
+    @event = current_user.events.find(params[:id])
+    if @event.update_attributes(item_params)
+      flash[:success] = "Event updated"
+      redirect_to @event
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @event = current_user.events.find(params[:id])
+    if @event
+      @event.destroy
+      flash[:success] = "Event removed"
+    else 
+      flash[:alert] = "Event removed"
+    end 
+    redirect_to root_path
   end
 
   private
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,5 @@ class Event < ApplicationRecord
 
     # belongs_to :user
     belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
+    validates :description, presence: true
 end
