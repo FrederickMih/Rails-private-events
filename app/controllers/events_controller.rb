ef4ee27cb7016a33d010fd8ab34f6efe374829d5
FrederickMih <fredtimermih@gmail.com>
2021-03-31 15:12:43 +0100

    Added the date field and create event page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -53,7 +53,7 @@ class EventsController < ApplicationController
   private
 
   def event_params
-    params.require(:event).permit( :description)
+    params.require(:event).permit( :description, :date)
   end
 end
 
--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,8 @@ class Event < ApplicationRecord
     # belongs_to :user
     belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
     validates :description, presence: true
+    validates :date, presence: true
+    
 
     
     has_many :event_users
