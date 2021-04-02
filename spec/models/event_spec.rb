fa797eb5527b16c66c3f93f8ebb353d7d3a121fa
ayman <aymanjaber2012@gmail.com>
2021-04-02 13:49:50 +0300

    Add last tests

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -14,6 +14,22 @@ RSpec.describe 'Event', type: :controller do
       expect(e2.valid?).to eq(false)
      end
 
+     it 'Checks that an event has a user' do
+      u1 = User.new(username: "John")
+      u1.save
+      e1 = Event.new(description: "Marriage celebration", user_id: u1.id, date: Date.today)
+      expect(e1.creator.username).to eq("John")
+    end
+
+    it 'Checks that an event is attended by at least a user' do
+      u1 = User.new(username: "John")
+      u1.save
+      e1 = Event.new(description: "Marriage celebration", user_id: u1.id, date: Date.today)
+      e1.save
+      e1.attendees << u1
+      expect(e1.attendees.first.username).to eq("John")
+    end
+
    end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,6 +16,45 @@ RSpec.describe 'User', type: :controller do
      e1 = u1.events.build(description: "something", date: Time.now) 
       expect(e1.valid?).to eq(true)
     end
+
+    it 'Checks that a user has not created the event' do
+      u2 = User.new(username: "Fred")
+      u1 = User.new(username: "John")
+      u1.save
+      u2.save
+      e1 = Event.new(description: "Marriage celebration", user_id: u1.id, date: Date.today)
+      expect(u2.events.size).to eq(0)
+    end
+
+    it 'Checks that a user has created an event' do
+      u2 = User.new(username: "Fred")
+      u1 = User.new(username: "John")
+      u1.save
+      u2.save
+      e1 = Event.new(description: "Marriage celebration", user_id: u1.id, date: Date.today)
+      e1.save
+      expect(u1.events.size).to eq(1)
+    end
+
+    it 'Checks that a user has been added to an event' do
+      u2 = User.new(username: "Fred")
+      u1 = User.new(username: "John")
+      u1.save
+      u2.save
+      e1 = Event.new(description: "Marriage celebration", user_id: u1.id, date: Date.today)
+      expect(u2.attended_events.size).to eq(0)
+    end
+
+    it 'Checks that a user has not been added to an event' do
+      u2 = User.new(username: "Fred")
+      u1 = User.new(username: "John")
+      u1.save
+      u2.save
+      e1 = Event.new(description: "Marriage celebration", user_id: u1.id, date: Date.today)
+      e1.save
+      u1.attended_events << e1
+      expect(u1.attended_events.size).to eq(1)
+    end
    
   end
 
